2cafdb8bb9190110e3fe7bff2092f6fd09109b4f
cafetolo72 <osorio21205@iesmarenostrum.com>
2021-06-10 17:07:47 +0200

    He realizado los cambios necesarios para mejorar el código

--- ClassLibrary1/ClassLibrary1/Examen3EV.cs
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -1,39 +1,75 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace Examen3EV_NS
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
-    {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int not;  // Notables
-        public int sbr;  // Sobresalientes
+    /// <summary>
+    /// Clase que calculará las Estadísticas de un conjunto de notas 
+    /// </summary>
 
-        public double media; // Nota media
+    public class EstadisticaNotasJOB2021  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    {
+        private int tSuspensos;       // Suspensos
+        private int tAprobados;       // Aprobados
+        private int tNotables;        // Notables
+        private int tSobresalientes;  // Sobresalientes
+        private double tMedia;        // Nota media
+
+        public const string listaVacia = "La lista no contiene ninguna nota";    // Excepcion lista vacía
+        public const string notasFueraRango = "Las notas en la lista son menores que 0 y mayores que 10"; // Excepción notas fuera de rango
+
+        public int SuspensosJOB2021                     // Devuelve todos los suspenso en la lista
+        {
+            get { return tSuspensos; }
+            set { tSuspensos = value; }
+        }
+
+        public int Aprobadosjob2021                     // Devuelve todos los aprobados por debajo de 7 de la lista
+        {
+            get { return tAprobados; }
+            set { tAprobados = value; }
+        }
+
+        public int NotablesJOB2021                // Devuelve todos los notables de la lista
+        {
+            get { return tNotables; }
+            set { tNotables = value; }
+        }
+
+        public int SobresalientesJOB2021                // Devuelve todos los sobresalientes de la lista 
+        {
+            get { return tSobresalientes; }
+            set { tSobresalientes = value; }
+        }
+
+        public double MediaJOB2021                      // Devuelve la media de todas las  notas 
+        {
+            get { return tMedia; }
+            set { tMedia = value; }
+        }
 
         // Constructor vacío
-        public estNot()
+
+        /// <summary>
+        /// Constructor vacío en el que ponene todos los atributos a 0.
+        /// </summary>
+
+        public EstadisticaNotasJOB2021()
         {
-            sus = apr = not = sbr = 0;  // inicializamos las variables
-            media = 0.0;
+            tSuspensos = tAprobados = tNotables = tSobresalientes = 0;  // inicializamos las variables
+            tMedia = 0.0;
         }
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+
+        /// <summary>
+        /// Constructor que rellena los métodos a partir de la función CalcularEstadistica así se validarán también las notas
+        /// </summary>
+        /// <param name="listaNotas"> Una lista que contiene notas que en este caso son double </param>
+
+        public EstadisticaNotasJOB2021(List<double> listaNotas)
         {
-            media = 0.0;
-
-            for (int i = 0; i < listnot.Count; i++)
-            {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
-            }
-
-            media = media / listnot.Count;
+            CalcularEstadisticaJOB2021(listaNotas);
         }
 
 
@@ -41,32 +77,44 @@ namespace Examen3EV_NS
         // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
         //
         // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
+
+        /// <summary>
+        /// Función que valida las notas, las separa en aprobados, suspensos, notables y sobresalientes
+        /// Y saca la media de todas las notas introducidas
+        /// </summary>
+        /// <exception cref="listaVacia"> Excepción que se produce cuando la lista no contiene nada </exception>
+        /// <exception cref="notasFueraRango"> Excepción que se produce cuando la lista comprende notas por encima de 10 o por debajo de 0 </exception>
+        /// <param name="listaNotas"> Una lista que contiene notas que en este caso son double </param>
+
+        public void CalcularEstadisticaJOB2021(List<double> listaNotas)
         {                                 
-            media = 0.0;
+            tMedia = 0.0;
 
             // TODO: hay que modificar el tratamiento de errores para generar excepciones
-            //
-            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
-                return -1;
+        
+            if (listaNotas.Count <= 0)                           // Si la lista no contiene elementos, devolvemos un error
+                throw new SystemException(listaVacia);
 
-            for (int i=0;i<10;i++)
-                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                return -1;
-
-            for (int i = 0; i < listnot.Count; i++)
+            for (int i = 0; i < listaNotas.Count; i++)
             {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
+                if (listaNotas[i] < 0 || listaNotas[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
+                    throw new ArgumentOutOfRangeException(notasFueraRango);
+            }
+            for (int i = 0; i < listaNotas.Count; i++)
+            {
+                if (listaNotas[i] < 5)
+                    tSuspensos++;                                        // Por debajo de 5 suspenso
+                else if (listaNotas[i] >= 5 && listaNotas[i] < 7)
+                    tAprobados++;                                        // Nota para aprobar: 5
+                else if (listaNotas[i] >= 7 && listaNotas[i] < 9)
+                    tNotables++;                                         // Nota para notable: 7
+                else if (listaNotas[i] >=9)
+                    tSobresalientes++;                                   // Nota para sobresaliente: 9
 
-                media = media + listnot[i];
+                tMedia += listaNotas[i];
             }
 
-            media = media / listnot.Count;
-
-            return media;
+            tMedia /=listaNotas.Count;
         }
     }
 }
--- ClassLibrary1/UnitTestProject1/UnitTest1.cs
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Examen3EV_NS;
+using System;
 
-namespace UnitTestProject1
+namespace UnitTestProject1JOB2021
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1JOB2021()
         {
-            List<int> notas = new List<int>();
-
+            List<double> notas = new List<double>();
+            
             notas.Add(0);
             notas.Add(5);
             notas.Add(9);
@@ -21,10 +22,62 @@ namespace UnitTestProject1
             notas.Add(8);
 
             double mediaEsperada = 5.143;
-            int susE = 3;
-            int aprE = 1;
-            int notE = 2;
-            int sbrE = 1;
+            int suspensosEsperados = 3;
+            int aprobadosEsperados = 1;
+            int notablesEsperados = 2;
+            int sobresalientesEesperados = 1;
+
+            EstadisticaNotasJOB2021 estadisticaNotas = new EstadisticaNotasJOB2021(notas);
+
+            int suspensos = estadisticaNotas.SuspensosJOB2021;
+            int aprobados = estadisticaNotas.Aprobadosjob2021;
+            int notables = estadisticaNotas.NotablesJOB2021;
+            int sobresalientes = estadisticaNotas.SobresalientesJOB2021;
+            double media = estadisticaNotas.MediaJOB2021;
+
+            Assert.AreEqual(suspensos, suspensosEsperados, 0.001, "El resultado no es el esperado");
+            Assert.AreEqual(aprobados, aprobadosEsperados, 0.001, "El resultado no es el esperado");
+            Assert.AreEqual(notables, notablesEsperados, 0.001, "El resultado no es el esperado");
+            Assert.AreEqual(sobresalientes, sobresalientesEesperados, 0.001, "El resultado no es el esperado");
+            Assert.AreEqual(media, mediaEsperada, 0.001, "El resultado no es el esperado");
+        }
+
+        [TestMethod]
+        public void PruebaErrorListaVaciaJOB2021()
+        {
+            List<double> notas = new List<double>();
+
+            try
+            {
+            EstadisticaNotasJOB2021 estadisticaNotas = new EstadisticaNotasJOB2021(notas);
+            }
+            catch(SystemException e)
+            {
+                StringAssert.Contains(e.Message, EstadisticaNotasJOB2021.listaVacia);
+                return;
+            }
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void PruebaErrorNotasFueraRangoJOB2021()
+        {
+            List<double> notas = new List<double>();
+            notas.Add(6);
+            notas.Add(12);
+            notas.Add(-1);
+            notas.Add(8);
+
+            try
+            {
+                EstadisticaNotasJOB2021 estadisticaNotas = new EstadisticaNotasJOB2021(notas);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, EstadisticaNotasJOB2021.notasFueraRango);
+                return;
+            }
+            Assert.Fail();
         }
     }
 }
